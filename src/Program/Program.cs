db617c0fdef9dbb31838f6bf0a118158f50904e0
ValentinaSilveira <62141464+ValentinaSilveira@users.noreply.github.com>
2020-06-09 20:36:40 -0300

    class Images

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -26,7 +26,7 @@ namespace Telegram.Bot.Examples.Echo
         /// Para probar este ejemplo, crea un bot nuevo y eeemplaza este 
         /// token por el de tu bot.
         /// </summary>
-        private static string Token = "1214554557:AAFMAslDQ-QTW7lKVaYE-SQ1LgiPp1SAvhs";
+        private static string Token = "1214554557:AAFMAslDQ-QTW7lKVaYE-SQ1LgiPp1SAvh";
 
         /// <summary>
         /// Punto de entrada.
@@ -49,6 +49,103 @@ namespace Telegram.Bot.Examples.Echo
 
             // Terminamos el bot.
             cts.Cancel();
+        }
+
+        /// <summary>
+        /// Maneja las actualizaciones del bot (todo lo que llega), incluyendo
+        /// mensajes, ediciones de mensajes, respuestas a botones, etc. En este
+        /// ejemplo sólo manejamos mensajes de texto.
+        /// </summary>
+        /// <param name="update"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task HandleUpdateAsync(Update update, CancellationToken cancellationToken)
+        {
+            try 
+            {
+                // sólo respondemos a mensajes de texto
+                if (update.Type == UpdateType.Message)
+                {
+                    await HandleMessageReceived(update.Message);
+                }
+            }
+            catch(Exception e)
+            {
+                await HandleErrorAsync(e, cancellationToken);
+            }
+        }
+
+        /// <summary>
+        /// Maneja los mensajes que se envían al bot.
+        /// Lo único que hacemos por ahora es escuchar 3 tipos de mensajes:
+        /// - "hola": responde con texto
+        /// - "chau": responde con texto
+        /// - "foto": responde con una foto
+        /// </summary>
+        /// <param name="message">El mensaje recibido</param>
+        /// <returns></returns>
+        private static async Task HandleMessageReceived(Message message)
+        {
+            Console.WriteLine($"Received a message from {message.From.FirstName} saying: {message.Text}");
+            
+            string response;
+
+            switch(message.Text.ToLower())
+            {
+                case "hola": 
+                    response = "hola! como estás?";
+                    break;
+
+                case "chau": 
+                    response = "Chau! Que andes bien!";
+                    break;
+
+                case "foto":
+                    // si nos piden una foto, mandamos la foto en vez de responder
+                    // con un mensaje de texto.
+                    await SendProfileImage(message);
+                    return;
+
+                default: 
+                    response = "Disculpa, no se qué hacer con ese mensaje!";
+                    break;
+            }
+
+            // enviamos el texto de respuesta
+            await Bot.SendTextMessageAsync(message.Chat.Id, response);
+        }
+
+        /// <summary>
+        /// Envía una imágen como respuesta al mensaje recibido.
+        /// Como ejemplo enviamos siempre la misma foto.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        static async Task SendProfileImage(Message message)
+        {
+            await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.UploadPhoto);
+
+            const string filePath = @"profile.jpeg";
+            using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            var fileName = filePath.Split(Path.DirectorySeparatorChar).Last();
+            await Bot.SendPhotoAsync(
+                chatId: message.Chat.Id,
+                photo: new InputOnlineFile(fileStream, fileName),
+                caption: "Te ves bien!"
+            );
+        }
+
+        /// <summary>
+        /// Manejo de excepciones. 
+        /// Por ahora simplemente la imprimimos en la consola.
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static Task HandleErrorAsync(Exception exception, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(exception.Message);
+            return Task.CompletedTask;
         }
     }
 }
